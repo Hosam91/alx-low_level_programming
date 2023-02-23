4471299fd3993af0152fc6411c7454f83ba97c3e
Hosam91 <hosammohamed9111@gmail.com>
2023-02-22 17:53:49 -0800

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *_abs - abs the value
+  *@num: number to checked
+  *
+  *Return: absolute numnber
+  */
+int _abs(int n)
+{
+	if (n <	0)
+	{
+		return (-n);
+	}
+	return (n);
+}
