c5f7801eeb52424928dda92119d7cb1c8397f72e
Hosam91 <hosammohamed9111@gmail.com>
2023-02-22 16:20:25 -0800

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 
 /**
   * _islower - check if lower or not
-  *   * @c: char which we will check it
-  *    
-  *          * Return: 1 if lower 0 or not
+  * @c: char which we will check it
+  *
+  * Return: 1 if lower 0 or not
   */
 int _islower(int c)
 {
-	if(c <= 'z' && c >= 'a')
+	if (c <= 'z' && c >= 'a')
 	{
 		return (1);
 	}
-	return(0);	
-		    
+	return (0);
 }
