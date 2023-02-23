a13242b2fa8ed05ca529006073655a17c6e95881
Hosam91 <hosammohamed9111@gmail.com>
2023-02-22 16:41:15 -0800

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *print_sign - print + if number is beager then 0 - if less then 0 0 if 0
+  *@n: number which we check
+  *
+  *Return: 1 if more then 1
+  */
+
+int print_sign(int n)
+{
+	if (n > 0 )
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
