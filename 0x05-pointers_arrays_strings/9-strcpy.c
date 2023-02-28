ead4dbd35b8802c58fc1c7e82aa62628d401b650
Hosam91 <hosammohamed9111@gmail.com>
2023-02-27 18:57:26 -0800

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strcpy - concat two string
+  * @dest: first string
+  * @src: second string
+  *
+  *Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
