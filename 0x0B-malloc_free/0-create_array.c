574ab0fa7be3b843588186215addf54e870cf2e3
Hosam91 <hosammohamed9111@gmail.com>
2023-03-14 15:42:38 -0700

    A

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create array
+ * @size: number of array
+ * @c: char
+ * Return: address of array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	int i;
+	int nsize;
+	
+	nsize = size;
+	if(size == 0)
+		return (0);
+	for (i = 0; i < nsize ; i++)
+	{
+		a[i] = c;
+	}
+	a[i]='\0';
+	return (0);
+}
